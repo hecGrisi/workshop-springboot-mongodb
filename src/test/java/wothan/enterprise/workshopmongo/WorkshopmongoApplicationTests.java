29749685032a5d5a69d86e024e5239a277442e62
Hector <hecgrisi@gmail.com>
2024-08-26 14:29:27 -0300

    Project created

---
+++ src/test/java/wothan/enterprise/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package wothan.enterprise.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
