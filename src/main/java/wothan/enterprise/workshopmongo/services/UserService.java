8cb651b5238db06bed5c47efce9ab01d714d2996
Hector <hecgrisi@gmail.com>
2024-08-27 11:39:07 -0300

    Getting a user by id

--- src/main/java/wothan/enterprise/workshopmongo/services/UserService.java
+++ src/main/java/wothan/enterprise/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package wothan.enterprise.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import wothan.enterprise.workshopmongo.domain.User;
 import wothan.enterprise.workshopmongo.repository.UserRepository;
+import wothan.enterprise.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
