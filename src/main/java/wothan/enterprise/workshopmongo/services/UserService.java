9c8205bd7d5f68eb5b4a629a42221374fe52935b
Hector <hecgrisi@gmail.com>
2024-08-27 14:06:38 -0300

    User insertion with POST

--- src/main/java/wothan/enterprise/workshopmongo/services/UserService.java
+++ src/main/java/wothan/enterprise/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import wothan.enterprise.workshopmongo.domain.User;
+import wothan.enterprise.workshopmongo.dto.UserDTO;
 import wothan.enterprise.workshopmongo.repository.UserRepository;
 import wothan.enterprise.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
 	}
 }
